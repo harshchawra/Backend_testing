083ca22b10b2862aeca6d752fd16c315797d309b
Harsh.ra <harsh2004.chawra@gmail.com>
2025-03-10 22:13:48 +0530

    With gitignore

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const {getDb} = require("../index.js");
 
 router.get("/", (req, res) => {
     res.json({msg : "API is working."});
+});
+
+router.get('/books' ,async (req,res) => {
+    const db = await getDb();
+    db.collection('books')
+        .find()
+        .sort()
+        .toArray()
+        .then((book) =>  {
+            res.json(book);
+        })
+        .catch(() => {
+            res.json({error : "DB error while fetching."});
+        })
 });
 
 module.exports = router;
